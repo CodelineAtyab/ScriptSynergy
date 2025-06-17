11cae2ea91e599af4a300f8dc8042fd306b4ca45
AsmaAljabri <aasmaa.aljabrii@gmail.com>
2025-06-17 12:17:41 +0400

    added function for print list and print even numbers from a list

---
+++ Examples/From_Asma/js/message_manager.js
@@ -0,0 +1,50 @@
+// const teamName = "Design Dynamos";
+// console.log(teamName.toUpperCase());
+// console.log(teamName);
+
+// let age = 30;
+
+// age = age + 1;
+
+
+// if (true) {
+//   {
+//     let currentDate = "2023-10-01";
+//     const NUMBER_OF_DAYS_IN_WEEK = 7;
+
+
+
+
+
+//   }
+// }
+
+
+
+// function printNum(num){
+//   console.log(num);
+// }
+
+// const num = [1,2,3,4,5,6,7,8,9,10];
+// num.forEach(printNum);
+
+//pointng p to an object 
+// const p = function printNum(num){
+//   console.log(num);
+// }
+
+
+// const arr = [2,4,6,8,10];
+// arr.forEach(p);
+
+const e = function printNum(num){
+const result =  (num % 2 == 0);
+
+return result;
+
+
+}
+
+
+const arr2 = [22,33,44,55,63,232,252,645,2,4,6,8,10];
+console.log(arr2.filter(e));
