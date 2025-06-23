e8505b687618a2872a2ffc6dfc83e20cbd43e915
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2025-06-23 15:33:16 +0400

    Updated the complain Admin View and Fix bugs

--- examples_src/from_atyab/VoiceVaultUI/js/populateComplainView.js
+++ examples_src/from_atyab/VoiceVaultUI/js/populateComplainView.js
@@ -1,29 +1,20 @@
 const complainViewDivObj = document.getElementById("complainViewDiv");
 
-document.getElementById("complaintList").addEventListener("change", (event) => {
-  // Handle complaint selection change
-  console.log(event);
-  const selectedValue = event.target.value;
-  const selectedOption = event.target.options[event.target.selectedIndex];
-  console.log("Selected complaint:", { 
-    value: selectedValue, 
-    text: selectedOption.text 
-  });
-
+function updateComplainView(title, complainId) {
   // Create needed child objects
   const newH1 = document.createElement("h1");
   newH1.innerHTML = "Complaint Information";
 
   const newH2 = document.createElement("h2");
-  newH2.innerHTML = selectedOption.text;
+  newH2.innerHTML = title;
 
   const newH3 = document.createElement("h3");
-  newH3.innerHTML = selectedValue;
+  newH3.innerHTML = complainId;
 
   const newPara = document.createElement("p");
 
   // Get complains from the API and populate the complain drop down
-  fetch(`http://localhost:9999/complaints/${selectedValue}`)
+  fetch(`http://localhost:9999/complaints/${complainId}`)
   .then((response) => response.json())
   .then((jsonComplainObject) => {
     newPara.innerHTML = jsonComplainObject.content;
@@ -36,4 +27,12 @@ document.getElementById("complaintList").addEventListener("change", (event) => {
   complainViewDivObj.appendChild(newH2);
   complainViewDivObj.appendChild(newH3);
   complainViewDivObj.appendChild(newPara);
+}
+
+document.getElementById("complaintList").addEventListener("change", (event) => {
+  // Handle complaint selection change
+  const selectedValue = event.target.value;
+  const selectedOption = event.target.options[event.target.selectedIndex];
+
+  updateComplainView(selectedOption.text, selectedValue);
 });
